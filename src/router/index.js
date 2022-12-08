ff7f41f2d40f324363f91956bf7c70856ab70751
huynk <khachuy469@gmail.com>
2022-12-08 23:38:17 +0700

    feat(be): build source base code backend

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+const ErrorHandle = require('../middleware/errorHandle');
+const AuthRouter = require('./authRouter');
+const UserRouter = require('./userRouter');
+const VoucherRouter = require('./voucherRouter');
+const CategoryRouter = require('./categoryRouter');
+const ProductRouter = require('./productRouter');
+const FlashSaleRouter = require('./flashSaleRouter');
+const OrderRouter = require('./orderRouter');
+const CommentRouter = require('./commentRouter');
+
+function route(app) {
+  app.use('/api/comment', CommentRouter);
+  app.use('/api/auth', AuthRouter);
+  app.use('/api/user', UserRouter);
+  app.use('/api/voucher', VoucherRouter);
+  app.use('/api/category', CategoryRouter);
+  app.use('/api/product', ProductRouter);
+  app.use('/api/flashsale', FlashSaleRouter);
+  app.use('/api/order', OrderRouter);
+
+  app.use(ErrorHandle);
+}
+
+module.exports = route;
